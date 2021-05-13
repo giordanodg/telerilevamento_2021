fc6a20bfea68fd262834a66b6f7622092f09a13f
giordanodg <80520226+giordanodg@users.noreply.github.com>
2021-05-13 13:30:39 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -4,5 +4,32 @@
 
 library(raster)
 
+setwd("/Users/Giordano/lab/greenland") 
+
+lst_2000 <- raster("lst_2000.tif")
+
+lst_2005 <- raster("lst_2005.tif")
+
+lst_2010 <- raster("lst_2010.tif")
+
+lst_2015 <- raster("lst_2015.tif")
+
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
  
-setwd("/Users/Giordano/Desktop/lab/greenland_data") 
+list.files()
+rlist <- list.files(pattern="lst")
+rlist
+
+
+import <- lapply(rlist,raster)
+import
+
+TGr <- stack(import)
+plot(TGr)
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
+
