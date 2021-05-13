64ac6ecac86f8461e29e95e7380b64cbdb41b805
giordanodg <80520226+giordanodg@users.noreply.github.com>
2021-05-13 17:24:17 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -33,3 +33,40 @@ plot(TGr)
 plotRGB(TGr, 1, 2, 3, stretch="Lin")
 plotRGB(TGr, 2, 3, 4, stretch="Lin")
 
+#installiamo e inseriamo il pacchetto library(rasterVis)
+
+install.packages("rasterVis")
+library(rasterVis)
+setwd("/Users/Giordano/lab/greenland")
+    
+rlist <- list.files(pattern="lst")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+TGr <- stack(import)
+TGr
+
+
+levelplot(TGr,col.regions=cl, main="LST variation in time",names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+#melt
+
+meltlist <- list.files(pattern="melt")
+melt_import <- lapply(meltlist,raster)
+melt <- stack(melt_import)
+melt
+ 
+
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+
+#il dollaro si utilizza per legare i file
+#creo una colorramppalette
+    
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+
+levelplot(melt_amount, col.regions=clb)
+ 
+
